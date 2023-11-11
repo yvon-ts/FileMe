2efaa4c98e0040076c4922407a745d48f96e2854
yvon <>
2023-11-11 14:57:23 +0800

    doc: 新增OpenApiConfig及瀏覽功能api doc

---
+++ src/main/java/net/fileme/domain/dto/DataOwnerDto.java
@@ -0,0 +1,21 @@
+package net.fileme.domain.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+public class DataOwnerDto {
+    @NotNull
+    @Schema(description = "用戶ID",
+            type = "string",
+            example = "1710573934860890113")
+    private Long userId;
+
+    @NotNull
+    @Schema(description = "檔案或目錄ID",
+            type = "string",
+            example = "1698350322036805633")
+    private Long dataId;
+}
